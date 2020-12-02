d14e2988bc9d4e58d386c94f2d40fd2d22ebc397
Jason Cao <41754906+caojason@users.noreply.github.com>
2020-12-01 17:14:36 -0800

    Add files via upload

---
+++ src/androidTest/java/com/example/goalstarterandroidapp/ExampleInstrumentedTest.java
@@ -0,0 +1,51 @@
+package com.example.goalstarterandroidapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+//    @Rule
+//    public ActivityScenarioRule<HostActivity> mActivityTestRule = new ActivityScenarioRule<HostActivity>(HostActivity.class);
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.goalstarterandroidapp", appContext.getPackageName());
+    }
+
+    
+
+
+}
---
+++ src/main/java/com/example/goalstarterandroidapp/FriendReqActivity.java
@@ -0,0 +1,84 @@
+package com.example.goalstarterandroidapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FriendReqActivity extends AppCompatActivity {
+
+    private static final String FRIENDREQLISTURL = "http://52.188.108.13:3000/home/pending/";
+    private FriendsReqRecycleViewAdapter mFriendsReqAdapter;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_req);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Context currentContext = this;
+
+
+        String userInfo = getIntent().getStringExtra("userInfo");
+        String userid = null;
+        try {
+            JSONObject data = new JSONObject(userInfo);
+            userid = data.getString("userid");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String requestURL = FRIENDREQLISTURL + userid;
+
+        RequestQueue queue = Volley.newRequestQueue(currentContext);
+
+        String finalUserid = userid;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, requestURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONArray responseArray = null;
+                        try {
+                            responseArray = new JSONArray(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("Friend REQ LIST" + responseArray);
+                        mFriendsReqAdapter = new FriendsReqRecycleViewAdapter(currentContext, responseArray, finalUserid);
+                        mRecyclerView = findViewById(R.id.recycler_view_friendreq_activity);
+                        mRecyclerView.setLayoutManager(new LinearLayoutManager(currentContext));
+                        BottomOffsetDecoration bottomOffsetDecoration = new BottomOffsetDecoration((int)(16 * getResources().getDisplayMetrics().density));
+                        mRecyclerView.addItemDecoration(bottomOffsetDecoration);
+                        mRecyclerView.setAdapter(mFriendsReqAdapter);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("ERROR RESPONSE");
+            }
+        });
+
+        queue.add(stringRequest);
+
+    }
+}
